ad356e8b4e5588b1f07c49b336c123748398cf25
mlarsen2 <mlarsen@insilico.us.com>
2020-09-09 16:47:17 -0400

    committing mavedb annotator

---
+++ annotators/mavedb/mavedb.py
@@ -0,0 +1,25 @@
+import sys
+from cravat import BaseAnnotator
+from cravat import InvalidData
+import sqlite3
+import os
+
+class CravatAnnotator(BaseAnnotator):
+    def annotate(self, input_data, secondary_data=None):
+        hugo = input_data['hugo']
+        achange = input_data['achange']
+        q = 'select score, accession from mavedb where hugo = "{hugo}" and hgvs_pro = "{achange}"'.format(
+            hugo = hugo, achange = achange)
+        self.cursor.execute(q)
+        row = self.cursor.fetchone()
+        accession = row[1]
+        if row:
+            out = {'score': row[0], 'accession': accession, 'vis': accession}
+        return out
+
+    def cleanup(self):
+        pass
+        
+if __name__ == '__main__':
+    annotator = CravatAnnotator(sys.argv)
+    annotator.run()
