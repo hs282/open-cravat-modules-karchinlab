68e6481d9da1174cb87da94987092cd956b30498
kmoad <kyle.moad@gmail.com>
2020-09-15 00:37:22 -0400

    23/ancestry/ftdna silently ignore ref variants

--- converters/23andme-converter/23andme-converter.py
+++ converters/23andme-converter/23andme-converter.py
@@ -66,6 +66,8 @@ class CravatConverter(BaseConverter):
                     'zygosity':zygosity,
                 }
                 ret.append(wdict)
-        if ret and zygosity == 'hom':
+        if not ret:
+            return self.IGNORE
+        if zygosity == 'hom':
             ret = ret[:1]
         return ret
