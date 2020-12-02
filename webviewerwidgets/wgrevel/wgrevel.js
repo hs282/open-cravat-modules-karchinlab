b82f95067af43aa89abb14fa1747bb0428c93cbb
mlarsen2 <mlarsen@insilico.us.com>
2020-12-02 15:30:28 -0500

    updated revel

--- webviewerwidgets/wgrevel/wgrevel.js
+++ webviewerwidgets/wgrevel/wgrevel.js
@@ -1,10 +1,26 @@
 widgetGenerators['revel'] = {
 	'variant': {
-		'width': 280, 
-		'height': 80, 
+		'width': 480, 
+		'height': 120, 
         'default_hidden': true,
 		'function': function (div, row, tabName) {
-			addInfoLine(div, 'Score', getWidgetData(tabName, 'revel', row, 'score'), tabName);
+			var allMappings = getWidgetData(tabName, 'revel', row, 'all');
+			if (allMappings != undefined && allMappings != null) {
+                var results = JSON.parse(allMappings);
+				var table = getWidgetTableFrame();
+				var thead = getWidgetTableHead(['Transcript', 'Score', 'Rank score']);
+				addEl(table, thead);
+				var tbody = getEl('tbody');
+                for (var i = 0; i < results.length; i++) {
+					var row = results[i];
+					var transcript = row[0];
+					var score = row[1];
+					var rankscore = row[2];
+					var tr = getWidgetTableTr([transcript, score, rankscore]);
+					addEl(tbody, tr);
+				}
+				addEl(div, addEl(table, tbody));
+			}
 		}
 	}
 }
