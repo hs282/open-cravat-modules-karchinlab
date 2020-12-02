7e88f9d32b49251f972bae92e7a532520e49f4e7
mlarsen2 <mlarsen@insilico.us.com>
2020-12-02 17:07:55 -0500

    updated dbsnps

--- annotators/dbsnp/dbsnp.py
+++ annotators/dbsnp/dbsnp.py
@@ -7,23 +7,17 @@ from cravat.util import get_ucsc_bins, reverse_complement
 
 class CravatAnnotator(BaseAnnotator):
 
-    def annotate(self, input_data):
-        out = None
-        chrom = input_data['chrom']
-        pos = input_data['pos']
-        ref_len = len(input_data['ref_base'])
-        alt = input_data['alt_base']
-        sql = ("SELECT snp FROM {} "
-                "WHERE pos='{}'"
-                " AND ref_len='{}'"
-                " AND alt='{}'").format(chrom, pos, ref_len, alt)
-        q = self.cursor.execute(sql)
-        snp_ids = ','.join(['rs' + str(i[0]) for i in q.fetchall()])
-        if snp_ids == "":
-            return out
-        out = {}
-        out['snp'] = snp_ids
-        return out
+    def annotate(self, input_data, secondary_data=None):
+        q = 'select snp from {chrom} where pos = {pos} and ref = "{ref}" and alt = "{alt}"'.format(
+            chrom = input_data["chrom"], pos = int(input_data["pos"]), ref = input_data["ref_base"], alt = input_data["alt_base"])
+        self.cursor.execute(q)
+        row = self.cursor.fetchone()
+        if row:
+            rsid = 'rs' + str(row[0])
+            return {'rsid': rsid}
+    
+    def cleanup(self):
+        pass
     
 if __name__ == '__main__':
     annotator = CravatAnnotator(sys.argv)
