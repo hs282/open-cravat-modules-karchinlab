0a28f2d053bbd112b2c2363e7bc764b2b8074247
Rick Kim <rkim@insilico.us.com>
2020-12-15 11:45:16 -0500

    wglollipop version up. cfgenesisreporter in progress. ndex_chd cardiovascular tag

--- reporters/cfgenesisreporter/cfgenesisreporter.py
+++ reporters/cfgenesisreporter/cfgenesisreporter.py
@@ -82,6 +82,25 @@ class Reporter(CravatReport):
         self.filename_postfix = 'cfgenesis.RData'
         self.data = {}
         self.wgs_reader = cravat.get_wgs_reader('hg38')
+        self.enstnov_ensgnov = {}
+        f = open(os.path.join(os.path.abspath(__file__), 'data', 'ensg_enst.txt'))
+        for line in f:
+            [ensg, enst] = line[:-1].split('\t')
+            self.enstnov_ensgnov[enst.split('.')[0]] = ensg.split('.')[0]
+        f.close()
+        f = open(os.path.join(os.path.abspath(__file__), 'data', 'MANE.GRCh38.v0.9.summary.txt'))
+        f.readline()
+        self.ensgnov_mane_enstnov = {}
+        for line in f:
+            [ensg, hugo, enst] = line[:-1].split('\t')
+            self.ensgnov_mane_enstnov[ensg.split('.')[0]] = enst.split('.')[0]
+        f.close()
+        f = open(os.path.join(os.path.abspath(__file__), 'data', 'enst_alen.txt'))
+        self.enstnov_alen = {}
+        for line in f:
+            [enst, alen] = line[:-1].split('\t')
+            self.enstnov_alen[enst.split('.')[0]] = int(alen)
+        f.close()
 
     def get_standardized_module_option(self, v):
         tv = type(v)
@@ -153,9 +172,15 @@ class Reporter(CravatReport):
             elif colname == 'extra_vcf_info__CSQ_Consequence':
                 self.colno_csq_consequence = colno
             elif colname == 'extra_vcf_info__CSQ_LoF':
-                self.colno_csq_lof = colno
+                self.colno_csq_lofs = colno
             elif colname == 'extra_vcf_info__CSQ_Gene':
                 self.colno_csq_gene = colno
+            elif colname == 'extra_vcf_info__CSQ_BIOTYPE':
+                self.colno_csq_biotype = colno
+            elif colname == 'base__transcript':
+                self.colno_transcript = colno
+            elif colname == 'base__all_mappings':
+                self.colno_all_mappings = colno
             colno += 1
         colno = 0
         self.colnos_to_display[level] = []
@@ -192,56 +217,91 @@ class Reporter(CravatReport):
             filtered_row[self.colno_to_display_pos] = pos
             filtered_row[self.colno_to_display_ref] = ref
             filtered_row[self.colno_to_display_alt] = alt
-        hugo = filtered_row[self.colno_to_display_hugo]
-        so = row[self.colno_so]
+        all_mappings = json.loads(filtered_row[self.colno_all_mappings])
+        hugo_enst = {}
+        for hugo in all_mappings:
+            mapping = all_mappings[0]
+            for mapping in all_mappings:
+                [_, _, sos, enst, _] = mapping
+                enstnov = enst.split('.')[0]
+                ensgnov = self.enstnov_ensgnov[enstnov]
+                if ensgnov in self.ensgnov_mane_enstnov:
+                    mane_enstnov = self.ensgnov_mane_enstnov[ensg_nov]
+                    if enstnov == mane_enstnov:
+                        hugo_enst[hugo] = enst
+                        break
+                else:
+                    
+
+
+
         coding = row[self.colno_coding]
+        genehancertargetgenes = row[self.colno_genehancertargetgenes]
         csq = row[self.colno_csq]
-        #csq_lof_tokno = 50
-        #csq_symbol_tokno = 3
-        #csq_consequence_tokno = 1
-        csq_lof = row[self.colno_csq_lof]
-        if csq_lof is None:
-            csq_lof = ''
-        csq_symbol = row[self.colno_csq_symbol]
-        if csq_symbol is None:
-            csq_symbol = ''
-        csq_symbol = csq_symbol.split(',')[0].split(';')[0]
+        csq_genes = row[self.colno_csq_gene]
+        csq_lofs = row[self.colno_csq_lofs]
         csq_consequence = row[self.colno_csq_consequence]
+        csq_biotypes = row[self.colno_csq_biotype]
         if csq_consequence is None:
-            csq_consequence = ''
-        csq_gene = row[self.colno_csq_gene]
-        if csq_gene is None:
-            csq_gene = ''
-        genenames = []
-        if csq_gene != '':
-            genenames = [m for v in csq_gene.split(',') for m in v.split(';')]
+            csq_consequence = []
+        else:
+            csq_consequence = csq_consequence.split(';')
+        '''
+        if csq_genes is not None:
+            csq_genes = [m for v in csq_genes.split(',') for m in v.split(';')]
         else:
             csq_toks = csq.split('|')
             for tok in csq_toks:
                 if 'ENSG' in tok:
-                    genenames = [m for v in tok.split(',') for m in v.split(';')]
-                elif tok == 'HC':
-                    csq_lof = tok
-        group_ids = []
-        if coding == 'Yes' or so == 'splice_site_variant' or 'HC' in csq_lof:
-            group_ids = genenames
-            #if hugo is not None and hugo != '':
-            #    group_id = hugo
-            #else:
-            #    group_id = csq_symbol
+                    csq_genes = [m for v in tok.split(',') for m in v.split(';')]
+                    break
+        if csq_genes is None:
+            csq_genes = []
+        '''
+        if csq_lofs is not None:
+            csq_lofs = csq_lofs.split(';')
         else:
-            genehancertargetgenes = row[self.colno_genehancertargetgenes]
+            csq_toks = csq.split('|')
+            for tok in csq_toks:
+                if 'HC' in tok:
+                    csq_lofs = tok.split(';')
+                    break
+        if csq_lofs is None:
+            csq_lofs = []
+        if csq_biotypes is None:
+            csq_biotypes = []
+        else:
+            csq_biotypes = csq_biotypes.split(';')
+        if len(csq_genes)
+        if len(csq_genes) != len(csq_biotypes) or len(csq_genes) != len(csq_lofs):
+            print(f'Different number of gene names, biotypes, and LoFs. Gene names={csq_genes} biotypes={csq_biotypes} LoFs={csq_lofs}. row={row}')
+            exit()
+        group_ids = []
+        if coding == 'Yes':
+            for i in range(len(csq_genes)):
+                if csq_biotypes[i] == 'protein_coding':
+                    group_ids.append(csq_genes[i])
+        elif so == 'splice_site_variant':
+            for i in range(len(csq_genes)):
+                if csq_biotypes[i] == 'protein_coding':
+                    group_ids.append(csq_genes[i])
+        elif 'HC' in csq_lofs:
+            for i in range(len(csq_genes)):
+                if csq_biotypes[i] == 'protein_coding' or csq_lofs[i] == 'HC':
+                    group_ids.append(csq_genes[i])
+        else:
             if genehancertargetgenes is not None:
-                #toks = genehancertargetgenes.split(',')
-                #group_id = toks[0].split(':')[0]
-                group_ids = genenames
+                group_ids = csq_genes
             else:
-                if 'upstream_gene_variant' in csq_consequence:
-                    #group_id = csq_symbol
-                    group_ids = genenames
+                if len(csq_consequence) != len(csq_genes):
+                    print(f'Different number of gene names and CSQ consequences. row={row}')
+                    exit()
+                for i in range(len(csq_genes)):
+                    if 'upstream_gene_variant' in csq_consequence[i]:
+                        group_ids.append(csq_genes[i])
         chrom = filtered_row[self.colno_to_display_chrom]
         if len(group_ids) == 0:
-            print(f'{chrom} {pos} {ref} {alt} {csq}')
+            print(f'No gene name for {chrom} {pos} {ref} {alt} {csq}')
         else:
             if chrom.startswith('chr'):
                 chrom = chrom[3:]
