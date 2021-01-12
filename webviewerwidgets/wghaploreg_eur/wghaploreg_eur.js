49931c54579f5f4c0eb570e54d813de4cfb21cf6
Kyle Moad <kyle@ip-172-31-5-48.ec2.internal>
2021-01-12 06:31:09 +0000

    haploreg widgets

--- webviewerwidgets/wghaploreg_eur/wghaploreg_eur.js
+++ webviewerwidgets/wghaploreg_eur/wghaploreg_eur.js
@@ -3,10 +3,27 @@ widgetGenerators['haploreg_eur'] = {
 		'width': 380, 
 		'height': 200, 
 		'function': function (div, row, tabName) {
-			if (infomgr.getColumnNo('variant', 'base__hugo')) {
-				var snps = getWidgetData(tabName, 'haploreg_eur', row, 'snps');
-				if (snps) {
-                    snps = JSON.parse(snps);
+			var allAnnots = getWidgetData(tabName, 'haploreg_eur', row, 'all');
+			if (allAnnots != undefined && allAnnots != null) {
+				allAnnots = JSON.parse(allAnnots);
+				if (allAnnots.length > 0) {
+					var table = getWidgetTableFrame();
+					table.style.tableLayout = 'auto';
+					table.style.width = '100%';
+					var thead = getWidgetTableHead(['rsID', 'R\u00b2', 'D\''], ['50%', '25%', '25%']);
+					addEl(table, thead);
+					var tbody = getEl('tbody');
+					for (var i=0; i<allAnnots.length; i++) {
+						var tr = getWidgetTableTr([allAnnots[i][0], allAnnots[i][1], allAnnots[i][2]]);
+						addEl(tbody, tr);
+					}
+					addEl(div, addEl(table, tbody));
+				}
+			} else {
+				var snps = getWidgetData(tabName, 'haploreg_eur', row, 'snps').split(',');
+				var r2s = getWidgetData(tabName, 'haploreg_eur', row, 'r2s').split(',').map(parseFloat);
+				var dprimes = getWidgetData(tabName, 'haploreg_eur', row, 'dprimes').split(',').map(parseFloat);
+				if (snps && r2s && dprimes) {
 					var table = getWidgetTableFrame();
                     table.style.tableLayout = 'auto';
 					table.style.width = '100%';
@@ -14,7 +31,7 @@ widgetGenerators['haploreg_eur'] = {
 					addEl(table, thead);
 					var tbody = getEl('tbody');
 					for (var i=0; i<snps.length; i++) {
-						var tr = getWidgetTableTr(snps[i]);
+						var tr = getWidgetTableTr([snps[i], r2s[i], dprimes[i]]);
 						addEl(tbody, tr);
 					}
 					addEl(div, addEl(table, tbody));
