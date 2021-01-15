79489259a3fdd7474d6827f90ca252942a1f3652
Kyle Moad <kyle@ip-172-31-71-98.ec2.internal>
2021-01-15 04:46:38 +0000

    pharmgkb 2.0.0, better table

--- annotators/pharmgkb/pharmgkb.py
+++ annotators/pharmgkb/pharmgkb.py
@@ -10,7 +10,6 @@ class CravatAnnotator(BaseAnnotator):
     def setup(self): 
         data_path = os.path.join(self.data_dir, 'pharmgkb.pickle')
         self.data = pickle.load(open(data_path,'rb'))
-
     
     def annotate(self, input_data, secondary_data=None):
         key = ':'.join(map(str, [
@@ -19,16 +18,30 @@ class CravatAnnotator(BaseAnnotator):
             input_data['ref_base'],
             input_data['alt_base'],
         ]))
-        d = self.data.get(key)
-        if d:
-            out = {}
-            out.update(d)
-            out['chemical'] = ';'.join(out['chemical'])
-            out['chemid'] = ';'.join(out['chemid'])
+        hits = self.data.get(key)
+        if hits:
+            pharmgkb_id = hits[0]['pharmgkb_id']
+            assocs = []
+            for hit in hits:
+                chemicals = hit['chemical']
+                chem_urls = [
+                    f'https://www.pharmgkb.org/chemical/{chemid}' 
+                    for chemid in hit['chemid']
+                ]
+                assocs.append([
+                    hit['pmid'],
+                    hit['pheno_cat'],
+                    hit['sig'],
+                    hit['notes'],
+                    hit['sentence'],
+                    list(zip(chemicals, chem_urls)),
+                ])
+            return {
+                'id': pharmgkb_id,
+                'drug_assoc': assocs,
+            }
         else:
             return None
-
-        return out
     
     def cleanup(self):
         pass
