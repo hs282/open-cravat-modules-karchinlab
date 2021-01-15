24c04f9cf65a7b8a3b6e664d6ebed86aa8c6a905
kmoad <kyle.moad@gmail.com>
2021-01-15 10:32:33 -0800

    new pharmgkb widget

--- webviewerwidgets/wgpharmgkb/wgpharmgkb.js
+++ webviewerwidgets/wgpharmgkb/wgpharmgkb.js
@@ -1,16 +1,53 @@
 widgetGenerators['pharmgkb'] = {
 	'variant': {
-		'width': 280, 
-		'height': 180, 
+		'width': 680, 
+		'height': 280, 
 		'function': function (div, row, tabName) {
 			var chemical = getWidgetData(tabName, 'pharmgkb', row, 'chemical');
-			var chemsDiv = getEl('div');
-			addEl(div, chemsDiv);
-			var titleSpan = getEl('span');
-			addEl(chemsDiv, titleSpan);
-			addEl(titleSpan, getTn('Chemicals: '));
-			titleSpan.style['font-weight'] = 'bold';
-			if (chemical != undefined && chemical != null && chemical.indexOf('[[') == 0) {
+			var assocs = getWidgetData(tabName, 'pharmgkb', row, 'drug_assoc');
+			if (assocs != undefined && assocs != null) {
+				var pharmId = getWidgetData(tabName, 'pharmgkb', row, 'id');
+				addInfoLineLink(div, 'Variant', pharmId, `https://pharmgkb.org/variant/${pharmId}`);
+				assocs = JSON.parse(assocs);
+				var table = getWidgetTableFrame();
+				addEl(div, table);
+				table.style.tableLayout = 'auto';
+				table.style.width = '100%';
+				var thead = getWidgetTableHead(
+					['Chemicals', 'Sentence','Category','Significant','Study', 'Notes'], 
+					['15%',       '30%',     '8%',     '10%',        '12%',   '25%'  ]
+				);
+				addEl(table, thead);
+				var tbody = getEl('tbody');
+				addEl(table, tbody);
+				for (let row of assocs) {
+					let pmLink = getEl('a');
+					pmLink.href = `https://pubmed.ncbi.nlm.nih.gov/${row[4]}`;
+					pmLink.innerText = row[4];
+					pmLink.target = '_blank';
+					let chemSpan = getEl('span');
+					for (let i=0; i<row[0].length; i++) {
+						let chemInfo = row[0][i];
+						let chemLink = getEl('a');
+						chemLink.innerText = chemInfo[0];
+						chemLink.href = chemInfo[1];
+						chemLink.target = '_blank';
+						addEl(chemSpan, chemLink);
+						if (i === row[0].length-2) {
+							addEl(chemSpan, getTn(', '));
+						}
+					}
+					var tr = elementsToWidgetTr([chemSpan, row[1], row[2], row[3], pmLink, row[5]]);
+					addEl(tbody, tr);
+				}
+				addEl(div, addEl(table, tbody));
+			} else if (chemical != undefined && chemical != null && chemical.indexOf('[[') == 0) {
+				var chemsDiv = getEl('div');
+				addEl(div, chemsDiv);
+				var titleSpan = getEl('span');
+				addEl(chemsDiv, titleSpan);
+				addEl(titleSpan, getTn('Chemicals: '));
+				titleSpan.style['font-weight'] = 'bold';
 				var chemicals = JSON.parse(chemical);
 				for (let i=0; i<chemicals.length; i++) {
                     var row = chemicals[i];
@@ -26,6 +63,7 @@ widgetGenerators['pharmgkb'] = {
 						addEl(chemsDiv, getTn(', '));
 					}
 				}
+				addInfoLine(div, 'Description', getWidgetData(tabName, 'pharmgkb', row, 'sentence'), tabName);
 			} else {
                 var chemical = getWidgetData(tabName, 'pharmgkb', row, 'chemical');
                 var chemid = getWidgetData(tabName, 'pharmgkb', row, 'chemid');
@@ -52,8 +90,8 @@ widgetGenerators['pharmgkb'] = {
                         }
                     }
                 }
+				addInfoLine(div, 'Description', getWidgetData(tabName, 'pharmgkb', row, 'sentence'), tabName);
             }
-			addInfoLine(div, 'Description', getWidgetData(tabName, 'pharmgkb', row, 'sentence'), tabName);
 		}
 	}
 }
