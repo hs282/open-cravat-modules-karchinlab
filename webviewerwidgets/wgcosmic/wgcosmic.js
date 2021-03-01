9d666d81d509fb09add8b687a632f1e94fc1a0ac
Rick Kim <rkim@insilico.us.com>
2021-03-01 14:08:19 -0500

    wgcosmic update

--- webviewerwidgets/wgcosmic/wgcosmic.js
+++ webviewerwidgets/wgcosmic/wgcosmic.js
@@ -9,13 +9,12 @@ widgetGenerators['cosmic'] = {
 			addInfoLine(div, 'Transcript', getWidgetData(tabName, 'cosmic', row, 'transcript'), tabName);
 			addInfoLine(div, 'Protein Change', getWidgetData(tabName, 'cosmic', row, 'protein_change'), tabName);
 			var vcTissue = getWidgetData(tabName, 'cosmic', row, 'variant_count_tissue');
-			if (vcTissue != undefined && vcTissue !== null && vcTissue.indexOf('[[') == 0) {
-                var results = JSON.parse(vcTissue);
+			if (vcTissue != undefined && vcTissue !== null && typeof(vcTissue) == 'object') {
+                var results = vcTissue;
 				var table = getWidgetTableFrame();
 				var thead = getWidgetTableHead(['Tissue', 'Count'],['85%','15%']);
 				addEl(table, thead);
 				var tbody = getEl('tbody');
-				var toks = vcTissue.split(';');
 				for (var i = 0; i < results.length; i++) {
                     var tr = getWidgetTableTr(results[i]);
                     addEl(tbody, tr);
