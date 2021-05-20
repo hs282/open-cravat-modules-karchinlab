b102e71f83b9f27da2fa1144f33d2a6e3f5d1e87
mlarsen2 <mlarsen@insilico.us.com>
2021-05-20 10:54:06 -0400

    fix to rankscore and gnomad widget

--- webviewerwidgets/wggnomad/wggnomad.js
+++ webviewerwidgets/wggnomad/wggnomad.js
@@ -3,15 +3,15 @@ widgetGenerators['gnomad'] = {
 		'width': 280, 
 		'height': 280, 
 		'function': function (div, row, tabName) {
-			addGradientStopBarComponent(div, row, 'Total', 'gnomad3__af', tabName);
-			addGradientStopBarComponent(div, row, 'African/African Amr', 'gnomad3__af_afr', tabName);
-			addGradientStopBarComponent(div, row, 'Ashkenazi', 'gnomad3__af_asj', tabName);
-			addGradientStopBarComponent(div, row, 'East Asn', 'gnomad3__af_eas', tabName);
-			addGradientStopBarComponent(div, row, 'Finnish Eur', 'gnomad3__af_fin', tabName);
-			addGradientStopBarComponent(div, row, 'Latino/ADmixed Amr', 'gnomad3__af_lat', tabName);
-			addGradientStopBarComponent(div, row, 'Non-Finnish Eur', 'gnomad3__af_nfe', tabName);
-			addGradientStopBarComponent(div, row, 'Other', 'gnomad3__af_oth', tabName);
-			addGradientStopBarComponent(div, row, 'South Asn', 'gnomad3__af_sas', tabName);
+			addGradientStopBarComponent(div, row, 'Total', 'gnomad__af', tabName);
+			addGradientStopBarComponent(div, row, 'African/African Amr', 'gnomad__af_afr', tabName);
+			addGradientStopBarComponent(div, row, 'Ashkenazi', 'gnomad__af_asj', tabName);
+			addGradientStopBarComponent(div, row, 'East Asn', 'gnomad__af_eas', tabName);
+			addGradientStopBarComponent(div, row, 'Finnish Eur', 'gnomad__af_fin', tabName);
+			addGradientStopBarComponent(div, row, 'Latino/ADmixed Amr', 'gnomad__af_lat', tabName);
+			addGradientStopBarComponent(div, row, 'Non-Finnish Eur', 'gnomad__af_nfe', tabName);
+			addGradientStopBarComponent(div, row, 'Other', 'gnomad__af_oth', tabName);
+			addGradientStopBarComponent(div, row, 'South Asn', 'gnomad__af_sas', tabName);
 		}
 	}
 }
